51a27968793effc93eafaa9282abbaedfb75f71b
yoonsung713 <sandyj0713@naver.com>
2021-11-04 23:54:33 +0900

    HAPPY2

--- app/src/main/java/com/example/happy2/MainActivity.java
+++ app/src/main/java/com/example/happy2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.happy2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClick(View view) {
+        Toast.makeText(getApplicationContext(), "imageClicked", Toast.LENGTH_SHORT).show();
     }
 }
