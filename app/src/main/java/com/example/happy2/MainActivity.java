be89f217a702b45ba5ab354446747330bdac46b3
yoonsung713 <sandyj0713@naver.com>
2021-11-10 21:13:47 +0900

    HAPPY2

--- app/src/main/java/com/example/happy2/MainActivity.java
+++ app/src/main/java/com/example/happy2/MainActivity.java
@@ -2,20 +2,66 @@ package com.example.happy2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+//   ImageView bdi_img;
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        bdi_img = findViewById(R.id.image_bdi);
+//        bdi_img.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//               Intent intent = new Intent(getApplicationContext(), bdi_java.class);
+//               startActivity(intent);
+//            }
+//        });
+//
+//    }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
 
 
-    public void onClick(View view) {
-        Toast.makeText(getApplicationContext(), "imageClicked", Toast.LENGTH_SHORT).show();
-    }
+        public void bdiClicked(View view) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naver.com/"));
+            startActivity(intent);
+        }
+
+        public void baiClicked(View view) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naver.com/"));
+            startActivity(intent);
+        }
+
+        public void gadClicked(View view) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://yeunjeong.involve.me/gad7"));
+            startActivity(intent);
+        }
+
+        public void seiClicked(View view) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naver.com/"));
+            startActivity(intent);
+        }
+
+        public void hanyangClicked(View view) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:031-400-4331"));
+            startActivity(intent);
+        }
+
+
 }
---
+++ app/src/main/java/com/example/happy2/bdi_java.java
@@ -0,0 +1,23 @@
+package com.example.happy2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class bdi_java extends Activity {
+
+    ImageView bdi_sub;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bdi_layout);
+
+
+
+    }
+}
+
